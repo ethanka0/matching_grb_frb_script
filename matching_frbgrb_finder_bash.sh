aa5e14573275369afdbe29ddbde4ea0fe175f1b3
ohdeeer <85923938+ohdeeer@users.noreply.github.com>
2021-07-15 11:03:52 +0800

    Update matching_frbgrb_finder_bash.sh

--- matching_frbgrb_finder_bash.sh
+++ matching_frbgrb_finder_bash.sh
@@ -2,11 +2,16 @@
 
 echo "Time uncertainty: "
 read u_time
+echo "Maximum energy (MEV) i.e. 1000000: "
+read e_max
+echo "Minimum energy (MeV) i.e. 30: "
+read e_min
+
 count=0
 name="trialoutput"
 fitstag=".fits"
 
-awk -F , 'NR >= 2 && NR <= 600 { print NR " " $3 " " $4 " " $5 " " $6 " " $8 }' chime4.csv |
+awk -F , 'NR >= 2 && NR <= 600 { print NR " " $3 " " $4 " " $5 " " $6 " " $8 }' chime4.csv | #change filename
 while read -r lineno col3 col4 col5 col6 col8
 do
 	if (( $(echo "$col4 > $col6" |bc) ))
@@ -21,7 +26,7 @@ do
 	count=$((count+1))
 	file_name="${name}${count}${fitstag}"
 	time_max=$(echo "$col8 + $u_time" | bc)
-	time_min=$(echo "$col8 - $u_time" | bc)
-	gtselect infile=lat_alldata_filtered.fits outfile=$file_name ra=$col3 dec=$col5 rad=$max_error evclass=128 evtype=3 tmin=$time_min tmax=$time_max emin=30 emax=1000000 zmax=180
+	gtselect infile=lat_alldata_filtered.fits outfile=$file_name ra=$col3 dec=$col5 rad=$max_error evclass=128 evtype=3 tmin=$col8 tmax=$time_max emin=$e_min emax=$e_max zmax=180
+	#change the filename at "infile"
 	echo "${count} / 600"
 done
